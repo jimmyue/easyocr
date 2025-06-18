81306f521bb130b39444d04251762d73252c37ad
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2025-06-18 11:18:46 +0800

    Update run.py

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ warnings.filterwarnings("ignore", message=".*CUDA.*|.*pin_memory.*")  # 隐藏CU
 ssl._create_default_https_context = ssl._create_unverified_context    # 解决HTTPS证书验证问题 (下载模型时会报错)
 reader = easyocr.Reader(['ch_sim','en'])                              # 您可以一次传递多种语言，但并非所有语言都可以一起使用。英语与每种语言都兼容，共享共同字符的语言通常彼此兼容
 result = reader.readtext('Captcha.jpg', detail = 0, paragraph=True)   # detail=0：获取简单输出 ； paragraph=True：将原始结果合并成易于阅读的段落
-print(result)
+for data in result:                                                   # 识别表格效果不行时可以一列一列截图识别后复制到Excel
+    print(data)
